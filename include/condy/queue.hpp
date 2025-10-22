5d34e77a533e167b6456a620abd4b29156c60dff
wokron <yi1tang.yang@gmail.com>
2025-10-22 22:15:58 +0800

    add RingQueue

---
+++ include/condy/queue.hpp
@@ -0,0 +1,78 @@
+#pragma once
+
+#include <atomic>
+#include <cassert>
+#include <cstddef>
+#include <memory>
+#include <optional>
+
+namespace condy {
+
+template <typename T> T round_up_pow2(T n) {
+    if (n == 0)
+        return 1;
+    n--;
+    for (size_t i = 1; i < sizeof(T) * 8; i *= 2) {
+        n |= n >> i;
+    }
+    n++;
+    return n;
+}
+
+template <typename T> class RingQueue {
+public:
+    RingQueue(size_t capacity)
+        : data_(std::make_unique<T[]>(round_up_pow2(capacity))),
+          mask_(round_up_pow2(capacity) - 1) {}
+
+public:
+    std::optional<T> try_dequeue() {
+        size_t head = head_.load(std::memory_order_relaxed);
+        size_t tail = tail_.load(std::memory_order_acquire);
+        if (head == tail) {
+            return std::nullopt;
+        }
+        T item = std::move(data_[head & mask_]);
+        head_.store(head + 1, std::memory_order_relaxed);
+        return item;
+    }
+
+    template <typename U> bool try_enqueue(U &&item) {
+        size_t tail = tail_.load(std::memory_order_relaxed);
+        size_t head = head_.load(std::memory_order_acquire);
+        if (((tail + 1) & mask_) == (head & mask_)) {
+            return false;
+        }
+        data_[tail & mask_] = std::forward<U>(item);
+        tail_.store(tail + 1, std::memory_order_release);
+        return true;
+    }
+
+    size_t size_unreliable() const {
+        size_t head = head_.load(std::memory_order_acquire);
+        size_t tail = tail_.load(std::memory_order_acquire);
+        assert(tail >= head);
+        return tail - head;
+    }
+
+private:
+    std::unique_ptr<T[]> data_;
+    size_t mask_;
+    std::atomic_size_t head_{0}, tail_{0};
+};
+
+template <typename T> class MultiWriterRingQueue : public RingQueue<T> {
+public:
+    using Base = RingQueue<T>;
+    using Base::Base;
+
+    template <typename U> bool try_enqueue(U &&item) {
+        std::lock_guard<std::mutex> lock(write_mutex_);
+        return Base::try_enqueue(std::forward<U>(item));
+    }
+
+private:
+    std::mutex write_mutex_;
+};
+
+} // namespace condy
