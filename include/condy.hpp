7855b63b9f9c5cc7068221ca0ccbf72adac11a4b
wokron <yi1tang.yang@gmail.com>
2025-10-22 19:58:17 +0800

    include every headers

--- include/condy.hpp
+++ include/condy.hpp
@@ -1,4 +1,14 @@
 #pragma once
 
-#include "condy/version.hpp" // IWYU pragma: export
-#include "condy/coro.hpp" // IWYU pragma: export
+#include "condy/async_operations.hpp"   // IWYU pragma: export
+#include "condy/awaiter_operations.hpp" // IWYU pragma: export
+#include "condy/awaiters.hpp"           // IWYU pragma: export
+#include "condy/condy_uring.hpp"        // IWYU pragma: export
+#include "condy/context.hpp"            // IWYU pragma: export
+#include "condy/coro.hpp"               // IWYU pragma: export
+#include "condy/event_loop.hpp"         // IWYU pragma: export
+#include "condy/finish_handles.hpp"     // IWYU pragma: export
+#include "condy/strategies.hpp"         // IWYU pragma: export
+#include "condy/task.hpp"               // IWYU pragma: export
+#include "condy/utils.hpp"              // IWYU pragma: export
+#include "condy/version.hpp"            // IWYU pragma: export
