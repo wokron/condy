2973a664a73b0c3221409590aec15faa524aa250
wokron <yi1tang.yang@gmail.com>
2025-10-31 16:55:03 +0800

    enable some test

--- tests/test_async_operations.cpp
+++ tests/test_async_operations.cpp
@@ -1,43 +1,57 @@
-// #include "condy/coro.hpp"
-// #include "condy/event_loop.hpp"
-// #include "condy/task.hpp"
-// #include <condy/async_operations.hpp>
-// #include <doctest/doctest.h>
+#include "condy/coro.hpp"
+#include <condy/async_operations.hpp>
+#include <doctest/doctest.h>
 
-// TEST_CASE("test async_operations - simple read write") {
-//     int pipe_fds[2];
-//     REQUIRE(pipe(pipe_fds) == 0);
+namespace {
 
-//     const char msg[] = "Hello, condy!";
-//     char buf[20] = {0};
+void event_loop(size_t &unfinished) {
+    auto *ring = condy::Context::current().ring();
+    while (unfinished > 0) {
+        ring->submit();
+        ring->reap_completions([&](io_uring_cqe *cqe) {
+            auto handle_ptr = static_cast<condy::OpFinishHandle *>(
+                io_uring_cqe_get_data(cqe));
+            handle_ptr->set_result(cqe->res);
+            (*handle_ptr)();
+        });
+    }
+}
 
-//     condy::EventLoop<condy::SimpleStrategy> loop(8);
+} // namespace
 
-//     size_t unfinished = 1;
-//     auto writer = [&]() -> condy::Coro<void> {
-//         int bytes_written =
-//             co_await condy::async_write(pipe_fds[1], msg, sizeof(msg), 0);
-//         REQUIRE(bytes_written == sizeof(msg));
-//     };
-//     auto reader = [&]() -> condy::Coro<void> {
-//         int bytes_read =
-//             co_await condy::async_read(pipe_fds[0], buf, sizeof(msg), 0);
-//         REQUIRE(bytes_read == sizeof(msg));
-//     };
+TEST_CASE("test async_operations - simple read write") {
+    int pipe_fds[2];
+    REQUIRE(pipe(pipe_fds) == 0);
 
-//     auto func = [&]() -> condy::Coro<void> {
-//         auto w = condy::co_spawn(writer());
-//         auto r = condy::co_spawn(reader());
-//         co_await std::move(w);
-//         co_await std::move(r);
-//         REQUIRE(std::string(buf) == std::string(msg));
-//         --unfinished;
-//     };
+    const char msg[] = "Hello, condy!";
+    char buf[20] = {0};
 
-//     auto coro = func();
-//     REQUIRE(unfinished == 1);
+    condy::Ring ring;
+    io_uring_params params{};
+    std::memset(&params, 0, sizeof(params));
+    ring.init(8, &params);
+    auto &context = condy::Context::current();
+    context.init(&ring);
 
-//     loop.run(std::move(coro));
+    size_t unfinished = 2;
+    auto writer = [&]() -> condy::Coro<void> {
+        int bytes_written =
+            co_await condy::async_write(pipe_fds[1], msg, sizeof(msg), 0);
+        REQUIRE(bytes_written == sizeof(msg));
+        --unfinished;
+    };
+    auto reader = [&]() -> condy::Coro<void> {
+        int bytes_read =
+            co_await condy::async_read(pipe_fds[0], buf, sizeof(msg), 0);
+        REQUIRE(bytes_read == sizeof(msg));
+        --unfinished;
+    };
 
-//     REQUIRE(unfinished == 0);
-// }
+    writer().release().resume();
+    reader().release().resume();
+    REQUIRE(unfinished == 2);
+
+    event_loop(unfinished);
+
+    REQUIRE(unfinished == 0);
+}
