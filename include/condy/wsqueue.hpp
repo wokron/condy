2f98db2db4b2dd6f482609dc6ad3dd1c9a16e040
wokron <yi1tang.yang@gmail.com>
2025-11-02 19:17:51 +0800

    add BoundedTaskQueue

--- include/condy/wsqueue.hpp
+++ include/condy/wsqueue.hpp
@@ -245,4 +245,202 @@ UnboundedTaskQueue<T>::resize_array(Array *a, int64_t b, int64_t t) {
     return tmp;
 }
 
+template <typename T, size_t LogSize = 8> class BoundedTaskQueue {
+
+    static_assert(std::is_pointer_v<T>, "T must be a pointer type");
+
+    constexpr static int64_t BufferSize = int64_t{1} << LogSize;
+    constexpr static int64_t BufferMask = (BufferSize - 1);
+
+    static_assert((BufferSize >= 2) && ((BufferSize & (BufferSize - 1)) == 0));
+
+    alignas(64) std::atomic<int64_t> _top{0};
+    alignas(64) std::atomic<int64_t> _bottom{0};
+    alignas(64) std::atomic<T> _buffer[BufferSize];
+
+public:
+    /**
+    @brief constructs the queue with a given capacity
+    */
+    BoundedTaskQueue() = default;
+
+    /**
+    @brief destructs the queue
+    */
+    ~BoundedTaskQueue() = default;
+
+    /**
+    @brief queries if the queue is empty at the time of this call
+    */
+    bool empty() const noexcept;
+
+    /**
+    @brief queries the number of items at the time of this call
+    */
+    size_t size() const noexcept;
+
+    /**
+    @brief queries the capacity of the queue
+    */
+    constexpr size_t capacity() const;
+
+    /**
+    @brief tries to insert an item to the queue
+
+    @tparam O data type
+    @param item the item to perfect-forward to the queue
+    @return `true` if the insertion succeed or `false` (queue is full)
+
+    Only the owner thread can insert an item to the queue.
+
+    */
+    template <typename O> bool try_push(O &&item);
+
+    /**
+    @brief tries to insert an item to the queue or invoke the callable if fails
+
+    @tparam O data type
+    @tparam C callable type
+    @param item the item to perfect-forward to the queue
+    @param on_full callable to invoke when the queue is full (insertion fails)
+
+    Only the owner thread can insert an item to the queue.
+
+    */
+    template <typename O, typename C> void push(O &&item, C &&on_full);
+
+    /**
+    @brief pops out an item from the queue
+
+    Only the owner thread can pop out an item from the queue.
+    The return can be a `nullptr` if this operation failed (empty queue).
+    */
+    T pop();
+
+    /**
+    @brief steals an item from the queue
+
+    Any threads can try to steal an item from the queue.
+    The return can be a `nullptr` if this operation failed (not necessary
+    empty).
+    */
+    T steal();
+};
+
+// Function: empty
+template <typename T, size_t LogSize>
+bool BoundedTaskQueue<T, LogSize>::empty() const noexcept {
+    int64_t t = _top.load(std::memory_order_relaxed);
+    int64_t b = _bottom.load(std::memory_order_relaxed);
+    return b <= t;
+}
+
+// Function: size
+template <typename T, size_t LogSize>
+size_t BoundedTaskQueue<T, LogSize>::size() const noexcept {
+    int64_t t = _top.load(std::memory_order_relaxed);
+    int64_t b = _bottom.load(std::memory_order_relaxed);
+    return static_cast<size_t>(b >= t ? b - t : 0);
+}
+
+// Function: try_push
+template <typename T, size_t LogSize>
+template <typename O>
+bool BoundedTaskQueue<T, LogSize>::try_push(O &&o) {
+
+    int64_t b = _bottom.load(std::memory_order_relaxed);
+    int64_t t = _top.load(std::memory_order_acquire);
+
+    // queue is full with one additional item (b-t+1)
+    if ((b - t) > BufferSize - 1) [[unlikely]] {
+        return false;
+    }
+
+    _buffer[b & BufferMask].store(std::forward<O>(o),
+                                  std::memory_order_relaxed);
+
+    std::atomic_thread_fence(std::memory_order_release);
+
+    // original paper uses relaxed here but tsa complains
+    _bottom.store(b + 1, std::memory_order_release);
+
+    return true;
+}
+
+// Function: push
+template <typename T, size_t LogSize>
+template <typename O, typename C>
+void BoundedTaskQueue<T, LogSize>::push(O &&o, C &&on_full) {
+
+    int64_t b = _bottom.load(std::memory_order_relaxed);
+    int64_t t = _top.load(std::memory_order_acquire);
+
+    // queue is full with one additional item (b-t+1)
+    if ((b - t) > BufferSize - 1) [[unlikely]] {
+        on_full();
+        return;
+    }
+
+    _buffer[b & BufferMask].store(std::forward<O>(o),
+                                  std::memory_order_relaxed);
+
+    std::atomic_thread_fence(std::memory_order_release);
+
+    // original paper uses relaxed here but tsa complains
+    _bottom.store(b + 1, std::memory_order_release);
+}
+
+// Function: pop
+template <typename T, size_t LogSize> T BoundedTaskQueue<T, LogSize>::pop() {
+
+    int64_t b = _bottom.load(std::memory_order_relaxed) - 1;
+    _bottom.store(b, std::memory_order_relaxed);
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    int64_t t = _top.load(std::memory_order_relaxed);
+
+    T item{nullptr};
+
+    if (t <= b) {
+        item = _buffer[b & BufferMask].load(std::memory_order_relaxed);
+        if (t == b) {
+            // the last item just got stolen
+            if (!_top.compare_exchange_strong(t, t + 1,
+                                              std::memory_order_seq_cst,
+                                              std::memory_order_relaxed)) {
+                item = nullptr;
+            }
+            _bottom.store(b + 1, std::memory_order_relaxed);
+        }
+    } else {
+        _bottom.store(b + 1, std::memory_order_relaxed);
+    }
+
+    return item;
+}
+
+// Function: steal
+template <typename T, size_t LogSize> T BoundedTaskQueue<T, LogSize>::steal() {
+    int64_t t = _top.load(std::memory_order_acquire);
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    int64_t b = _bottom.load(std::memory_order_acquire);
+
+    T item{nullptr};
+
+    if (t < b) {
+        item = _buffer[t & BufferMask].load(std::memory_order_relaxed);
+        if (!_top.compare_exchange_strong(t, t + 1, std::memory_order_seq_cst,
+                                          std::memory_order_relaxed)) {
+            return nullptr;
+        }
+    }
+
+    return item;
+}
+
+// Function: capacity
+template <typename T, size_t LogSize>
+constexpr size_t BoundedTaskQueue<T, LogSize>::capacity() const {
+    return static_cast<size_t>(BufferSize);
+}
+
 } // namespace condy
