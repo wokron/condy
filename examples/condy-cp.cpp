5fcf85dbe5365baa8dec178507991561c0bb19d8
wokron <yi1tang.yang@gmail.com>
2025-11-05 20:23:22 +0800

    update cp example

--- examples/condy-cp.cpp
+++ examples/condy-cp.cpp
@@ -1,54 +1,57 @@
-#include "condy/async_operations.hpp"
-#include "condy/runtime.hpp"
-#include "condy/runtime_options.hpp"
 #include <condy.hpp>
 #include <cstddef>
+#include <cstring>
 #include <fcntl.h>
 #include <sys/ioctl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-int get_file_size(int fd, off_t *size) {
-    struct stat st;
+constexpr size_t TASK_NUM = 64;
+constexpr size_t BLOCK_ALIGN = 512;
+constexpr size_t CHUNK_SIZE = 256 * 1024 * 1024; // 256 MiB
 
-    if (fstat(fd, &st) < 0)
-        return -1;
-    if (S_ISREG(st.st_mode)) {
-        *size = st.st_size;
-        return 0;
-    } else if (S_ISBLK(st.st_mode)) {
-        unsigned long long bytes;
-
-        if (ioctl(fd, BLKGETSIZE64, &bytes) != 0)
-            return -1;
-
-        *size = bytes;
-        return 0;
-    }
-
-    return -1;
-}
+struct raw_deleter {
+    void operator()(void *ptr) const { std::free(ptr); }
+};
 
 condy::Coro<> copy_file_task(int infd, int outfd, off_t &offset,
-                             off_t file_size, size_t chunk_size) {
+                             off_t file_size) {
     using condy::operators::operator>>;
-    std::unique_ptr<char[]> buffer = std::make_unique<char[]>(chunk_size);
+
+    void *raw_ptr;
+    if (posix_memalign(&raw_ptr, BLOCK_ALIGN, CHUNK_SIZE) != 0) {
+        std::fprintf(stderr, "posix_memalign failed\n");
+        exit(1);
+    }
+    std::unique_ptr<char[], raw_deleter> buffer(static_cast<char *>(raw_ptr));
 
     while (offset < file_size) {
         off_t current_offset = offset;
-        size_t to_copy =
-            std::min(chunk_size, static_cast<size_t>(file_size - offset));
-        offset += to_copy;
+        bool partial_copy = current_offset + CHUNK_SIZE > file_size;
+        offset += CHUNK_SIZE;
 
-        auto [r1, r2] = co_await (
-            condy::async_read(infd, buffer.get(), to_copy, current_offset) >>
-            condy::async_write(outfd, buffer.get(), to_copy, current_offset));
-        if (r1 < 0 || r2 < 0) {
-            std::fprintf(stderr,
-                         "Error during file copy at offset %ld: "
-                         "read %s, write %s\n",
-                         current_offset, r1 < 0 ? std::strerror(-r1) : "ok",
-                         r2 < 0 ? std::strerror(-r2) : "ok");
+        int r1, r2;
+        if (!partial_copy) {
+            std::tie(r1, r2) =
+                co_await (condy::async_read(infd, buffer.get(), CHUNK_SIZE,
+                                            current_offset) >>
+                          condy::async_write(outfd, buffer.get(), CHUNK_SIZE,
+                                             current_offset));
+
+        } else {
+            r1 = co_await condy::async_read(infd, buffer.get(), CHUNK_SIZE,
+                                            current_offset);
+            r2 = co_await condy::async_write(outfd, buffer.get(), CHUNK_SIZE,
+                                             current_offset);
+        }
+        if (r1 < 0) {
+            std::fprintf(stderr, "Read error at offset %lld: %s\n",
+                         (long long)current_offset, std::strerror(-r1));
+            exit(1);
+        }
+        if (r2 < 0) {
+            std::fprintf(stderr, "Write error at offset %lld: %s\n",
+                         (long long)current_offset, std::strerror(-r2));
             exit(1);
         }
     }
@@ -58,12 +61,12 @@ condy::Coro<> co_main(const char *infile, const char *outfile) {
     using condy::operators::operator&&;
 
     struct statx statx_buf;
-    auto [infd, outfd, r] =
-        co_await (condy::async_openat(AT_FDCWD, infile, O_RDONLY, 0) &&
-                  condy::async_openat(AT_FDCWD, outfile,
-                                      O_WRONLY | O_CREAT | O_TRUNC, 0644) &&
-                  condy::async_statx(AT_FDCWD, infile, AT_STATX_SYNC_AS_STAT,
-                                     STATX_SIZE, &statx_buf));
+    auto [infd, outfd, r] = co_await (
+        condy::async_openat(AT_FDCWD, infile, O_RDONLY | O_DIRECT, 0) &&
+        condy::async_openat(AT_FDCWD, outfile,
+                            O_WRONLY | O_CREAT | O_TRUNC | O_DIRECT, 0644) &&
+        condy::async_statx(AT_FDCWD, infile, AT_STATX_SYNC_AS_STAT, STATX_SIZE,
+                           &statx_buf));
     if (infd < 0) {
         std::fprintf(stderr, "Failed to open input file '%s': %s\n", infile,
                      std::strerror(-infd));
@@ -79,20 +82,22 @@ condy::Coro<> co_main(const char *infile, const char *outfile) {
                      std::strerror(-r));
         exit(1);
     }
-    off_t file_size = statx_buf.stx_size;
 
+    off_t file_size = statx_buf.stx_size;
     off_t offset = 0;
-    constexpr size_t CHUNK_SIZE = 32 * 1024; // 32 KB
 
     std::vector<condy::Task<>> tasks;
-    for (int i = 0; i < 32; i++) {
-        tasks.emplace_back(condy::co_spawn(
-            copy_file_task(infd, outfd, offset, file_size, CHUNK_SIZE)));
+    for (int i = 0; i < TASK_NUM; i++) {
+        tasks.emplace_back(
+            condy::co_spawn(copy_file_task(infd, outfd, offset, file_size)));
     }
 
     for (auto &task : tasks) {
         co_await std::move(task);
     }
+
+    ftruncate(outfd, file_size);
+    co_await (condy::async_close(infd) && condy::async_close(outfd));
 }
 
 int main(int argc, char **argv) {
@@ -101,9 +106,10 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    auto options =
-        condy::SingleThreadOptions().sq_size(64).cq_size(128).submit_batch_size(
-            64);
+    auto options = condy::SingleThreadOptions()
+                       .sq_size(2 * TASK_NUM)
+                       .cq_size(4 * TASK_NUM)
+                       .submit_batch_size(2 * TASK_NUM);
     condy::SingleThreadRuntime runtime(options);
     condy::sync_wait(runtime, co_main(argv[1], argv[2]));
     return 0;
