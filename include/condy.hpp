6581de205f50361d98a1a74cf147bb2d6cc1a7ab
wokron <yi1tang.yang@gmail.com>
2025-11-19 20:32:13 +0800

    cleanup headers

--- include/condy.hpp
+++ include/condy.hpp
@@ -2,20 +2,12 @@
 
 #include "condy/async_operations.hpp"   // IWYU pragma: export
 #include "condy/awaiter_operations.hpp" // IWYU pragma: export
-#include "condy/awaiters.hpp"           // IWYU pragma: export
+#include "condy/buffers.hpp"            // IWYU pragma: export
 #include "condy/channel.hpp"            // IWYU pragma: export
-#include "condy/condy_uring.hpp"        // IWYU pragma: export
-#include "condy/context.hpp"            // IWYU pragma: export
 #include "condy/coro.hpp"               // IWYU pragma: export
-#include "condy/coro.inl"               // IWYU pragma: export
-#include "condy/finish_handles.hpp"     // IWYU pragma: export
-#include "condy/intrusive.hpp"          // IWYU pragma: export
-#include "condy/invoker.hpp"            // IWYU pragma: export
-#include "condy/ring.hpp"               // IWYU pragma: export
 #include "condy/runtime.hpp"            // IWYU pragma: export
 #include "condy/runtime_options.hpp"    // IWYU pragma: export
-#include "condy/singleton.hpp"          // IWYU pragma: export
 #include "condy/sync_wait.hpp"          // IWYU pragma: export
 #include "condy/task.hpp"               // IWYU pragma: export
-#include "condy/utils.hpp"              // IWYU pragma: export
+#include "condy/timer.hpp"              // IWYU pragma: export
 #include "condy/version.hpp"            // IWYU pragma: export
