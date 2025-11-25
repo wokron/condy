6bd0cf1891f8876e4c0186aac78858f0be21d578
wokron <yi1tang.yang@gmail.com>
2025-11-25 19:50:45 +0800

    add post bench

---
+++ benchmarks/post.cpp
@@ -0,0 +1,22 @@
+#include <condy.hpp>
+
+condy::Coro<void> test_post(size_t times) {
+    for (size_t i = 0; i < times; ++i) {
+        co_await condy::co_switch(condy::current_runtime());
+    }
+}
+
+int main() {
+    const size_t times = 10'000'000;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    condy::sync_wait(test_post(times));
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto duration =
+        std::chrono::duration_cast<std::chrono::nanoseconds>(end - start)
+            .count();
+    printf("Performed %zu context switches in %ld ns\n", times, duration);
+    printf("Average time per context switch: %.2f ns\n",
+           static_cast<double>(duration) / times);
+}
