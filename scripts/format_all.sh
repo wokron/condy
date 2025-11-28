c28bca3e59fd91812eeb3e841928dff49a4d8cd6
wokron <yi1tang.yang@gmail.com>
2025-11-28 15:39:44 +0800

    add format script

---
+++ scripts/format_all.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+usage() {
+    echo "Usage: $0 [-i]"
+    echo "Format all C++ source files in the project."
+    echo "  -h    Show this help message."
+    echo "  -i    Apply formatting changes in place. If not set, performs a dry run."
+    exit 1
+}
+
+IN_PLACE=0
+
+while getopts "ih" opt; do
+    case $opt in
+        i)
+            IN_PLACE=1
+            ;;
+        h)
+            usage
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+ALL_FILES=$(find ./ \( -name "*.cpp" -o -name "*.hpp" \) -type f \
+        -not -path "./third_party/*" -not -path "./build/*")
+
+if [ $IN_PLACE -eq 1 ]; then
+    echo "Formatting files..."
+    clang-format -i $ALL_FILES
+    echo "Complete."
+else
+    clang-format --dry-run --Werror $ALL_FILES
+    if [ $? -eq 0 ]; then
+        echo "Nothing to format."
+    fi
+fi
