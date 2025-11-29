1c6b36aa83e67eae79d5ce97fcd6a1f5789eb361
wokron <yi1tang.yang@gmail.com>
2025-11-29 22:17:31 +0800

    add SubmittedBufferQueueImpl

--- tests/test_buffers.cpp
+++ tests/test_buffers.cpp
@@ -1,5 +1,6 @@
 #include "condy/buffers.hpp"
 #include "condy/ring.hpp"
+#include <cerrno>
 #include <cstddef>
 #include <doctest/doctest.h>
 #include <liburing.h>
@@ -54,6 +55,96 @@ TEST_CASE("test buffers - ProvidedBuffersImpl buffer select") {
 
     char *buf = reinterpret_cast<char *>(impl.get_buffer(bid));
     REQUIRE(std::memcmp(buf, "test", 4) == 0);
+}
+
+TEST_CASE("test buffers - SubmittedBufferQueueImpl construct") {
+    condy::Ring ring;
+    io_uring_params params{};
+    std::memset(&params, 0, sizeof(params));
+    ring.init(8, &params);
+    condy::detail::SubmittedBufferQueueImpl impl(ring.ring(), 0, 4, 0);
+}
+
+TEST_CASE("test buffers - SubmittedBufferQueueImpl buffer destruct") {
+    condy::Ring ring;
+    io_uring_params params{};
+    std::memset(&params, 0, sizeof(params));
+    ring.init(8, &params);
+
+    auto impl = std::make_shared<condy::detail::ProvidedBufferPoolImpl>(
+        ring.ring(), 0, 1, 4, 0);
+
+    int pipefd[2];
+    REQUIRE(pipe(pipefd) == 0);
+
+    int r;
+
+    r = ::write(pipefd[1], "test", 4);
+    REQUIRE(r == 4);
+
+    r = ::write(pipefd[1], "test", 4);
+    REQUIRE(r == 4);
+
+    auto prep_read = [&] {
+        auto *sqe = ring.get_sqe();
+        io_uring_prep_read(sqe, pipefd[0], nullptr, 0, 0);
+        io_uring_sqe_set_flags(sqe, IOSQE_BUFFER_SELECT);
+        sqe->buf_group = 0;
+        io_uring_sqe_set_data(sqe, nullptr);
+    };
+
+    condy::detail::SubmittedBufferQueueImpl queue_impl(ring.ring(), 1, 1, 0);
+    prep_read();
+    prep_read();
+
+    size_t reaped = 0;
+    while (reaped < 2) {
+        reaped += ring.reap_completions(
+            [&](io_uring_cqe *cqe) {
+                auto *data = io_uring_cqe_get_data(cqe);
+                REQUIRE(data == nullptr);
+                int r = cqe->res;
+                REQUIRE(r > 0);
+                REQUIRE((cqe->flags & IORING_CQE_F_BUFFER));
+                size_t bid = cqe->flags >> IORING_CQE_BUFFER_SHIFT;
+                condy::ProvidedBuffer buffer(impl, impl->get_buffer(bid),
+                                             impl->buffer_size());
+                queue_impl.submit_buffer(std::move(buffer));
+            },
+            true);
+    }
+
+    r = ::write(pipefd[1], "test", 4);
+    REQUIRE(r == 4);
+
+    prep_read();
+    reaped = 0;
+    while (reaped < 1) {
+        ring.submit();
+        reaped += ring.reap_completions([&](io_uring_cqe *cqe) {
+            auto *data = io_uring_cqe_get_data(cqe);
+            REQUIRE(data == nullptr);
+            int r = cqe->res;
+            REQUIRE(r == -ENOBUFS);
+        });
+    }
+
+    queue_impl.remove_buffer(0);
+
+    prep_read();
+    reaped = 0;
+    while (reaped < 1) {
+        ring.submit();
+        reaped += ring.reap_completions([&](io_uring_cqe *cqe) {
+            auto *data = io_uring_cqe_get_data(cqe);
+            REQUIRE(data == nullptr);
+            int r = cqe->res;
+            REQUIRE(r > 0);
+            REQUIRE((cqe->flags & IORING_CQE_F_BUFFER));
+            size_t bid = cqe->flags >> IORING_CQE_BUFFER_SHIFT;
+            REQUIRE(bid == 0);
+        });
+    }
 }
 
 TEST_CASE("test buffers - buffer mutable/const") {
