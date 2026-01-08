909100124a6526fcc18c3df5b205fce63fe8b00c
wokron <yi1tang.yang@gmail.com>
2026-01-08 10:05:43 +0800

    update version to v1.0.0

--- include/condy/version.hpp
+++ include/condy/version.hpp
@@ -4,7 +4,6 @@
 
 #pragma once
 
-// TODO: Set version before first release
-#define CONDY_VERSION_MAJOR 0
+#define CONDY_VERSION_MAJOR 1
 #define CONDY_VERSION_MINOR 0
 #define CONDY_VERSION_PATCH 0
