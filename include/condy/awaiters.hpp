cdca0812b77b37282b0bb779ce6014aeb9c02d51
wokron <yi1tang.yang@gmail.com>
2026-02-12 16:48:05 +0800

    minor change

--- include/condy/awaiters.hpp
+++ include/condy/awaiters.hpp
@@ -69,6 +69,7 @@ public:
         context.runtime()->pend_work();
 
         io_uring_sqe *sqe = prep_func_(ring);
+        assert(sqe && "prep_func must return a valid sqe");
         sqe->flags |= static_cast<uint8_t>(flags);
         io_uring_sqe_set_data(
             sqe, encode_work(&finish_handle_.get(), WorkType::Common));
@@ -100,7 +101,7 @@ public:
     OpAwaiter(PrepFunc func, Args &&...args)
         : Base(HandleBox(
                    OpFinishHandle<CQEHandler>(std::forward<Args>(args)...)),
-               func) {}
+               std::move(func)) {}
 };
 
 template <PrepFuncLike PrepFunc, CQEHandlerLike CQEHandler,
@@ -117,7 +118,7 @@ public:
                        Args &&...args)
         : Base(HandleBox(MultiShotOpFinishHandle<CQEHandler, MultiShotFunc>(
                    std::move(multishot_func), std::forward<Args>(args)...)),
-               func) {}
+               std::move(func)) {}
 };
 
 template <PrepFuncLike PrepFunc, CQEHandlerLike CQEHandler, typename FreeFunc>
@@ -131,7 +132,7 @@ public:
     ZeroCopyOpAwaiter(PrepFunc func, FreeFunc free_func, Args &&...args)
         : Base(HandleBox(ZeroCopyOpFinishHandle<CQEHandler, FreeFunc>(
                    std::move(free_func), std::forward<Args>(args)...)),
-               func) {}
+               std::move(func)) {}
 };
 
 template <unsigned int Flags, AwaiterLike Awaiter>
