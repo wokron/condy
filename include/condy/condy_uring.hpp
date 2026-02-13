f2928221ecfb9eb163ff0109d0d6eef100484641
wokron <yi1tang.yang@gmail.com>
2026-02-13 17:17:14 +0800

    remove version check of async_uring_cmd

--- include/condy/condy_uring.hpp
+++ include/condy/condy_uring.hpp
@@ -21,3 +21,17 @@
     (major > IO_URING_VERSION_MAJOR ||                                         \
      (major == IO_URING_VERSION_MAJOR && minor > IO_URING_VERSION_MINOR))
 #endif
+
+// Polyfill for io_uring_prep_uring_cmd (added in liburing 2.13)
+// Opcode exists since 2.3, only the helper function is missing
+#if IO_URING_CHECK_VERSION(2, 13) // < 2.13
+inline void io_uring_prep_uring_cmd(struct io_uring_sqe *sqe, int cmd_op,
+                                    int fd) noexcept {
+    sqe->opcode = (__u8)IORING_OP_URING_CMD;
+    sqe->fd = fd;
+    sqe->cmd_op = cmd_op;
+    sqe->__pad1 = 0;
+    sqe->addr = 0ul;
+    sqe->len = 0;
+}
+#endif
