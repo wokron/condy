67d94d6f2a5a03d5348abb952ee5d46a5404e0ad
wokron <yi1tang.yang@gmail.com>
2026-02-23 22:29:06 +0800

    add ZeroCopyRxCQEHandler

--- include/condy/cqe_handler.hpp
+++ include/condy/cqe_handler.hpp
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "condy/concepts.hpp"
+#include "condy/zcrx.hpp"
 #include <cassert>
 #include <cerrno>
 #include <cstdint>
@@ -145,5 +146,25 @@ private:
     TxTimestampResult result_;
 };
 #endif
+
+class ZeroCopyRxCQEHandler {
+public:
+    using ReturnType = std::pair<int, ZeroCopyRxBuffer>;
+
+    ZeroCopyRxCQEHandler(ZeroCopyRxBufferPool *pool) : pool_(pool) {}
+
+    void handle_cqe(io_uring_cqe *cqe) {
+        assert(detail::check_cqe32(cqe) &&
+               "Expected big CQE for zero-copy RX operations");
+        result_.first = cqe->res;
+        result_.second = pool_->handle_finish(cqe);
+    }
+
+    ReturnType extract_result() { return std::move(result_); }
+
+private:
+    ReturnType result_;
+    ZeroCopyRxBufferPool *pool_;
+};
 
 } // namespace condy
