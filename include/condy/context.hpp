17a8ed6ed77c922306aed142fe6a088fcec6b86f
wokron <yi1tang.yang@gmail.com>
2026-03-11 19:30:42 +0800

    mark noexcept for Context

--- include/condy/context.hpp
+++ include/condy/context.hpp
@@ -19,24 +19,24 @@ namespace detail {
 
 class Context : public ThreadLocalSingleton<Context> {
 public:
-    void init(Ring *ring, Runtime *runtime) {
+    void init(Ring *ring, Runtime *runtime) noexcept {
         ring_ = ring;
         runtime_ = runtime;
         bgid_pool_.reset();
     }
-    void reset() {
+    void reset() noexcept {
         ring_ = nullptr;
         runtime_ = nullptr;
         bgid_pool_.reset();
     }
 
-    Ring *ring() { return ring_; }
+    Ring *ring() noexcept { return ring_; }
 
-    Runtime *runtime() { return runtime_; }
+    Runtime *runtime() noexcept { return runtime_; }
 
     uint16_t next_bgid() { return bgid_pool_.allocate(); }
 
-    void recycle_bgid(uint16_t bgid) { bgid_pool_.recycle(bgid); }
+    void recycle_bgid(uint16_t bgid) noexcept { bgid_pool_.recycle(bgid); }
 
 private:
     Ring *ring_ = nullptr;
